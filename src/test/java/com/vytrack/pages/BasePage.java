77ee99a3f411afcedd1d16689113aab6b3fdf5ba
serkanserdarusa <serkanserdarusa@gmail.com>
2020-04-05 01:56:19 -0400

    commit

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -13,8 +13,8 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
+//burasi master
+//serkan
 //Mehmet some changes from online
 
 //serkan some changes from local
