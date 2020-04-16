2ed36ba89097875fa64a0ae996f6b2a460e5ed08
serkanserdarusa <61401437+serkanserdarusa@users.noreply.github.com>
2020-04-15 23:42:59 -0400

    Update BasePage.java

--- src/test/java/com/vytrack/pages/BasePage.java
+++ src/test/java/com/vytrack/pages/BasePage.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
+//whatsupp serkan
 //everything that is in common among pages
 //can go here
 //for example top menu elements don't belong to specific page
